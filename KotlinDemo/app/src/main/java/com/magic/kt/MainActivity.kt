e91071f0a0d783b6aa0e2e043fe46a8f44cf9413
yzp531 <yzp531@163.com>
2018-09-06 20:33:25 +0800

    no message

--- KotlinDemo/app/src/main/java/com/magic/kt/MainActivity.kt
+++ KotlinDemo/app/src/main/java/com/magic/kt/MainActivity.kt
@@ -6,16 +6,11 @@ import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener{
+class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        test.setOnClickListener(this);
-    }
-
-    override fun onClick(p0: View?) {
-        Toast.makeText(this@MainActivity,R.string.app_name,Toast.LENGTH_LONG).show();
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        test.setOnClickListener({Toast.makeText(this@MainActivity,R.string.app_name,Toast.LENGTH_LONG).show()})
     }
 }
